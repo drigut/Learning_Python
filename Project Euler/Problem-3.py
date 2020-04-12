7dfe35ce27a2ed8e8c3519e84f6765e8424a000b
Jack <badmadjack@ya.ru>
2020-04-12 11:41:03 +0300

    Solve Problem-3

--- Project Euler/Problem-3.py
+++ Project Euler/Problem-3.py
@@ -7,8 +7,24 @@ What is the largest prime factor of the number 600851475143 ?
 """
 
 
-def prime(n):
-    pass
+def is_prime(n):
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(n * 0.5) + 1, 2):
+        print(i)
+        if n % i == 0:
+            return False
+    return True
 
 
-print(prime(13195))
+def primes(n):
+    factors = list()
+    for i in range(3, int(n / 2) + 1, 2):
+        print(i)
+        if n % i == 0 and is_prime(i):
+            factors.append(i)
+        print(factors)
+    return factors
+
+
+print(max(primes(600851475143)))
